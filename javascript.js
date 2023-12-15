8f8ef5cdab0dc134377a01af66fd6fe5645afc8b
jtcopes23 <jtcopes23@gmail.com>
2023-12-14 23:00:28 -0800

    javascript conditional statements

---
+++ javascript.js
@@ -0,0 +1,44 @@
+/**** conditional statements javascript ****/
+/*if, else if, else */
+console.log('JOSHUA')
+
+let x = 'nf';
+if (x < 10){
+    console.log("x is less than 10")
+}else if(x > 10){
+    console.log("x is greater than 10")
+}else if(x > 10){
+    console.log("x is greater than 10")
+}else if(x > 10){
+    console.log("x is greater than 10")
+}else if(x > 10){
+    console.log("x is greater than 10")
+}else if(x > 10){
+    console.log("x is greater than 10")
+}else if(x > 10){
+    console.log("x is greater than 10")
+}else if(x > 10){
+    console.log("x is greater than 10")
+}
+else{
+    console.log("bye")
+}
+/* switch javascript */
+
+let age = 25 ;
+switch(age){
+    case 10 :
+        console.log('you are a minor');
+        break;
+
+    case 20:
+        console.log('you are a teenager');
+        break;
+
+    case 25:
+        console.log('you are an adult');
+        break;
+
+    default:
+        console.log('they didnt gave proper age');
+}
